2cc3d52e43fe5c55f4806096e108aed9346a6346
myketheguru <darkmyketheguru@gmail.com>
2025-02-16 18:09:36 +0100

    Trigger Updates

--- src/pubsub/subscribers/redis-subscriber.ts
+++ src/pubsub/subscribers/redis-subscriber.ts
@@ -3,9 +3,9 @@ import { redisService } from '../RedisService.js';
 console.log('Starting Redis event listener...');
 
 redisService.subscribe('ocpp:client_updates', (message) => {
-  console.log('Client Update Event:', message);
+  console.log('Client Update Event#:', message);
 });
 
 redisService.subscribe('ocpp:events', (message) => {
-  console.log('OCPP Event:', message);
+  console.log('OCPP Event#:', message);
 });
